0245a70a75e3a05d828758f881dbfb1230cd0734
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-05-15 16:16:20 +0530

    delete operation successfull

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -41,6 +41,13 @@ async function run() {
       const user = req.body;
       console.log("current user is", user);
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+    // DELETE REQUEST
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
       res.send(result);
     });
 
